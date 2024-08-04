c6a3e895291d87f1950919c59aac8c11be334d5c
Dikiy Perec <122711062+Drygoy@users.noreply.github.com>
2024-08-04 20:29:57 +0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,14 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fade: {
+          '0%': { opacity: "0"},
+          '100%': { opacity: "1"},
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
